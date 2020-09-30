32c2f06ce97f66922e512cdccdc7129756535eb7
addubinski <addubinski@gmail.com>
2020-09-30 22:25:18 +0900

    add tests for missing aruments

--- test/test.py
+++ test/test.py
@@ -6,21 +6,35 @@ import filecmp
 class TestCLI(unittest.TestCase):
 
     def test_required_arguments(self):
+
         result_file = open('test_result.txt', 'w+')
         process = subprocess.Popen(['get-n-largest'], shell=True, stdout=subprocess.PIPE, stderr=result_file)
         stdout, _ = process.communicate()
         self.assertFalse(bool(stdout))
         self.assertTrue(filecmp.cmp('test_result.txt', 'test_reference_files/missing_url.txt'),
-                        'Stderr reference for missing url argument did not match.')
+                        'Stderr reference for missing argument "URL" did not match.')
         result_file.close()
+
         result_file = open('test_result.txt', 'w+')
         process = subprocess.Popen(['get-n-largest', 'https://dummy-path.com/test.txt'],
                                    shell=True, stdout=subprocess.PIPE, stderr=result_file)
         stdout, _ = process.communicate()
         self.assertFalse(bool(stdout))
         self.assertTrue(filecmp.cmp('test_result.txt', 'test_reference_files/missing_n.txt'),
-                        'Stderr reference for missing n argument did not match.')
+                        'Stderr reference for missing argument "N" did not match.')
         result_file.close()
+
+        result_file = open('test_result.txt', 'w+')
+        process = subprocess.Popen(['n-largest-set-cache'],
+                                   shell=True, stdout=subprocess.PIPE, stderr=result_file)
+        stdout, _ = process.communicate()
+        self.assertFalse(bool(stdout))
+        self.assertTrue(filecmp.cmp('test_result.txt', 'test_reference_files/set_cache_missing_url.txt'),
+                        'Stderr reference for missing argument "ABSOLUTE_PATH" did not match.')
+        result_file.close()
+
+
+
 
 
 class TestGetNLargest(unittest.TestCase):
